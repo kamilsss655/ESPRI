7e574f842314a91222f3fcdb9e0c1c5ae52303a6
Nunu <kamilsos@gmail.com>
2024-03-05 11:01:32 +0100

    Docker clean-up more frequent.

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # first clean images older than 24h, you will run out of disk space one day
-docker image prune -a --force --filter "until=24h"
+docker image prune -a --force --filter "until=8h"
 docker build -t esp .
 docker run --rm -v ${PWD}:/project -w /project esp idf.py build
