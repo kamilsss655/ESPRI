ec462258be9ffb88dad92e5311bd2e9bf84f6882
Nunu <kamilsos@gmail.com>
2024-03-07 22:58:59 +0100

    remove unused headers from router

--- main/web/router.c
+++ main/web/router.c
@@ -14,18 +14,6 @@
  *     limitations under the License.
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/param.h>
-#include <sys/unistd.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <esp_err.h>
-#include <esp_log.h>
-#include <esp_vfs.h>
-#include <esp_spiffs.h>
-#include <esp_http_server.h>
-
 #include "helper/http.h"
 #include "web/handlers/root_handler.h"
 #include "web/handlers/static_files_handler.h"
