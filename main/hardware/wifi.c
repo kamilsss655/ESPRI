8c73b15874ca235fa3bad5da63aeacba3c5fbcc5
Nunu <kamilsos@gmail.com>
2024-03-08 15:36:53 +0100

    rename wifi sta event handler

--- main/hardware/wifi.c
+++ main/hardware/wifi.c
@@ -66,7 +66,7 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
     }
 }
 
-static void event_handler(void* arg, esp_event_base_t event_base,
+static void WIFI_sta_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -121,12 +121,12 @@ void wifi_init_sta(void)
     esp_event_handler_instance_t instance_got_ip;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
-                                                        &event_handler,
+                                                        &WIFI_sta_event_handler,
                                                         NULL,
                                                         &instance_any_id));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
+                                                        &WIFI_sta_event_handler,
                                                         NULL,
                                                         &instance_got_ip));
 
