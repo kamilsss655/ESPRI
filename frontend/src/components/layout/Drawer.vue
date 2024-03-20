c05adf74baa6c044bc83a4d5188dac1da1416a55
Nunu <kamilsos@gmail.com>
2024-03-21 00:54:07 +0100

    drawer links

--- frontend/src/components/layout/Drawer.vue
+++ frontend/src/components/layout/Drawer.vue
@@ -1,6 +1,34 @@
 <template>
   <q-drawer v-model="store.leftDrawerIsOpen" side="left" flat>
-    <!-- drawer content -->
+    <q-list>
+        <q-item clickable to="/" rel="noopener">
+          <q-item-section avatar>
+            <q-icon name="ion-home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Home</q-item-label>
+            <q-item-label caption>Main home page</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/settings" rel="noopener">
+          <q-item-section avatar>
+            <q-icon name="ion-settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+            <q-item-label caption>Manage settings</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable to="/about" rel="noopener">
+          <q-item-section avatar>
+            <q-icon name="ion-information-circle" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>About</q-item-label>
+            <q-item-label caption>More info about the project</q-item-label>
+          </q-item-section>
+        </q-item>
+    </q-list>
   </q-drawer>
 </template>
 
