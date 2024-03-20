c12c6d5a8ce843eecfb1ddbcd3ce0cb8cabe4d2b
Nunu <kamilsos@gmail.com>
2024-03-20 20:25:53 +0100

    settings wifi password hidden

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -11,27 +11,53 @@
           v-model.number="settingsStore['wifi.mode']"
           type="number"
           label="Mode"
+          hint="0 for OFF, 1 for AP, 2 for STA"
+          filled
         />
-        <q-input v-model="settingsStore['wifi.ssid']" label="SSID" />
-        <q-input v-model="settingsStore['wifi.password']" label="Password" />
+        <q-input v-model="settingsStore['wifi.ssid']" label="SSID" filled />
+
+        <q-input
+          v-model="settingsStore['wifi.password']"
+          label="Password"
+          filled
+          :type="hideWifiPassword ? 'password' : 'text'"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="hideWifiPassword ? 'ion-eye' : 'ion-eye-off'"
+              class="cursor-pointer"
+              @click="hideWifiPassword = !hideWifiPassword"
+            />
+          </template>
+        </q-input>
+
         <q-input
           v-model.number="settingsStore['wifi.channel']"
           type="number"
           label="Channel"
+          filled
         />
         <q-input
           v-model.number="settingsStore['gpio.status_led']"
           type="number"
           label="Status LED"
+          filled
         />
         <q-input
           v-model.number="settingsStore['gpio.audio_out']"
           type="number"
           label="Audio OUT"
+          filled
         />
         <div class="text-right q-pa-md">
           <q-btn icon="ion-play" label="Submit" type="submit" color="primary" />
-          <q-btn icon="ion-refresh" label="Reload" type="reset" class="q-ml-sm" outline />
+          <q-btn
+            icon="ion-refresh"
+            label="Reload"
+            type="reset"
+            class="q-ml-sm"
+            outline
+          />
         </div>
       </q-form>
     </q-card-section>
@@ -39,10 +65,11 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useSettingsStore } from "../stores/settings";
 import { useQuasar } from "quasar";
 
+const hideWifiPassword = ref(true);
 const settingsStore = useSettingsStore();
 const $q = useQuasar();
 
